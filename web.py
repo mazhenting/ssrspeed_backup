d47d95066dca92ed1f21d5eaf8b1df974f07ba11
mazhenting <mazhenting44@gmail.com>
2019-08-30 19:51:30 +0800

    2019-08-30

--- web.py
+++ web.py
@@ -8,21 +8,23 @@ import threading
 import urllib.parse
 import logging
 
+from config import config
+
 from flask import Flask,request,redirect#,render_template
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 
-from SSRSpeed.Utils.RequirementCheck.RequireCheck import RequirementCheck
-from SSRSpeed.Utils.checkPlatform import checkPlatform
+from ssrspeed.utils import RequirementsCheck, check_platform
+from ssrspeed.utils.web import getPostData
 
-from SSRSpeed.Utils.Web.getpostdata import getPostData
+from ssrspeed.core.ssrspeed_core import SSRSpeedCore
+from ssrspeed.shell import web_cli as console_cfg
 
-from SSRSpeed.Core.SSRSpeedCore import SSRSpeedCore
-import SSRSpeed.Core.Shell.ConsoleWeb as ShellWebServer
+from ssrspeed.result import ExportResult
+from ssrspeed.result import importResult
 
-from SSRSpeed.Result.exportResult import ExportResult
-import SSRSpeed.Result.importResult as importResult
-
-from config import config
+from ssrspeed.types.errors.webapi.error_file_not_allowed import FileNotAllowed
+from ssrspeed.types.errors.webapi.error_file_common import WebFileCommonError
 
 WEB_API_VERSION = config["WEB_API_VERSION"]
 
@@ -45,18 +47,22 @@ consoleHandler.setFormatter(formatter)
 
 TEMPLATE_FOLDER = "./resources/webui/templates"
 STATIC_FOLDER = "./resources/webui/statics"
+UPLOAD_FOLDER = "./tmp/uploads"
+ALLOWED_EXTENSIONS = set(["json", "yml"])
 
 app = Flask(__name__,
 	template_folder=TEMPLATE_FOLDER,
 	static_folder=STATIC_FOLDER,
 	static_url_path=""
-	)
+)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 CORS(app)
 sc = None
 
 @app.route("/",methods=["GET"])
 def index():
-	return redirect("https://web.绒布球.site/",301)
+	return redirect("https://web.绒布球.site/", 301)
 	#return render_template(
 	#	"index.html"
 	#	)
@@ -65,7 +71,7 @@ def index():
 	{
 		"proxyType":"SSR", //[SSR,SSR-C#,SS,V2RAY]
 		"testMethod":"SOCKET", //[SOCKET,SPEED_TEST_NET,FAST]
-		"testMode":"",//[ALL,TCP_PING]
+		"testMode":"",//[ALL,TCP_PING,WEB_PAGE_SIMULATION]
 		"subscriptionUrl":"",
 		"colors":"origin",
 		"sortMethod":"",//[SPEED,REVERSE_SPEED,PING,REVERSE_PING]
@@ -89,65 +95,92 @@ def getVersion():
 
 @app.route("/status",methods=["GET"])
 def status():
-	return sc.webGetStatus()
+	return sc.web_get_status()
 
 @app.route("/readsubscriptions",methods=["POST"])
 def readSubscriptions():
 	if (request.method == "POST"):
 		data = getPostData()
-		if (sc.webGetStatus() == "running"):
+		if (sc.web_get_status() == "running"):
 			return 'running'
 		subscriptionUrl = data.get("url","")
-		proxyType = data.get("proxyType","SSR")
+		#proxyType = data.get("proxyType","SSR")
 		if (not subscriptionUrl):
 			return "invalid url."
-		return json.dumps(sc.webReadSubscription(subscriptionUrl,proxyType))
+		return json.dumps(sc.web_read_subscription(subscriptionUrl))
+
+def check_file_allowed(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/readfileconfig", methods=["POST"])
+def readFileConfig():
+	if request.method == "POST":
+		if (sc.web_get_status() == "running"):
+			return 'running'
+		ufile = request.files["file"]
+		#data = getPostData()
+		if ufile:
+			if check_file_allowed(ufile.filename):
+				filename = secure_filename(ufile.filename)
+				tmpFilename = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+				ufile.save(tmpFilename)
+				logger.info("Tmp config file saved as {}".format(tmpFilename))
+				return json.dumps(sc.web_read_config_file(tmpFilename))
+			else:
+				logger.error("Disallowed file {}".format(ufile.filename))
+				return FileNotAllowed.errMsg
+		else:
+			logger.error("File upload failed or unknown error.")
+			return WebFileCommonError.errMsg
 
 @app.route("/getcolors",methods=["GET"])
 def getColors():
-	return json.dumps(sc.webGetColors())
+	return json.dumps(sc.web_get_colors())
 
 @app.route('/start',methods=["POST"])
 def startTest():
 	if (request.method == "POST"):
 		data = getPostData()
 	#	return "SUCCESS"
-		if (sc.webGetStatus() == "running"):
+		if (sc.web_get_status() == "running"):
 			return 'running'
 		configs = data.get("configs",[])
 		if (not configs):
 			return "No configs"
-		proxyType =data.get("proxyType","SSR")
-		testMethod =data.get("testMethod","SOCKET")
-		colors =data.get("colors","origin")
-		sortMethod =data.get("sortMethod","")
-		testMode = data.get("testMode","")
-		sc.webSetup(
+		#proxyType =data.get("proxyType","SSR")
+		testMethod =data.get("testMethod", "ST_ASYNC")
+		colors =data.get("colors", "origin")
+		sortMethod =data.get("sortMethod", "")
+		testMode = data.get("testMode", "")
+		use_ssr_cs = data.get("useSsrCSharp", False)
+		group = data.get("group", "")
+		sc.web_setup(
 			testMode = testMode,
 			testMethod = testMethod,
 			colors = colors,
-			sortMethod = sortMethod,
-			proxyType = proxyType
+			sortMethod = sortMethod
 		)
-		sc.cleanResults()
-		sc.webSetConfigs(configs)
-		sc.startTest()
+		sc.clean_result()
+		sc.web_set_configs(configs)
+		if group:
+			sc.set_group(group)
+		sc.start_test(use_ssr_cs)
 		return 'done'
 	return 'invalid method'
 
 @app.route('/getresults')
 def getResults():
-	return json.dumps(sc.webGetResults())
+	return json.dumps(sc.web_get_results())
 
 if (__name__ == "__main__"):
-	pfInfo = checkPlatform()
+	pfInfo = check_platform()
 	if (pfInfo == "Unknown"):
 		logger.critical("Your system does not supported.Please contact developer.")
 		sys.exit(1)
 
 	DEBUG = False
 	
-	options,args = ShellWebServer.init(WEB_API_VERSION)
+	options,args = console_cfg.init(WEB_API_VERSION)
 
 	if (options.paolu):
 		for root, dirs, files in os.walk(".", topdown=False):
@@ -175,13 +208,21 @@ if (__name__ == "__main__"):
 			item.addHandler(fileHandler)
 			item.addHandler(consoleHandler)
 
+	logger.info("SSRSpeed {}, Web Api Version {}".format(config["VERSION"], config["WEB_API_VERSION"]))
+
 	if (logger.level == logging.DEBUG):
 		logger.debug("Program running in debug mode")
 
-	rc = RequirementCheck()
-	rc.check()
+	if not options.skip_requirements_check:
+		rc = RequirementsCheck()
+		rc.check()
+	else:
+		logger.warn("Requirements check skipped.")
 
 	sc = SSRSpeedCore()
 	sc.webMode = True
+	if not os.path.exists(UPLOAD_FOLDER):
+		logger.warn("Upload folder {} not found, creating.".format(UPLOAD_FOLDER))
+		os.makedirs(UPLOAD_FOLDER)
 	app.run(host=options.listen,port=int(options.port),debug=DEBUG,threaded=True)
 
